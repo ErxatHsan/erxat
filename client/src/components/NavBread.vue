def78e3f860b3d03c7f33b5269ab06b417c524d8
ErxatHsan <510997123@qq.com>
2017-11-02 17:14:46 +0800

    a

--- client/src/components/NavBread.vue
+++ client/src/components/NavBread.vue
@@ -2,7 +2,7 @@
     <div class="nav-breadcrumb-wrap">
         <div class="container">
             <nav class="nav-breadcrumb">
-                <a href="/">主页</a>
+                <a href="/">特别的名字</a>
                 <slot></slot>
             </nav>
         </div>
