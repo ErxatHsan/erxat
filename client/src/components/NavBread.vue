30fe5cf57416e10f5c60a3144d33a1b2dd430605
ErxatHsan <510997123@qq.com>
2017-11-03 16:19:48 +0800

    a

--- client/src/components/NavBread.vue
+++ client/src/components/NavBread.vue
@@ -2,7 +2,7 @@
     <div class="nav-breadcrumb-wrap">
         <div class="container">
             <nav class="nav-breadcrumb">
-                <a href="/">依尔夏提</a>
+                <a href="/">啊啊啊</a>
                 <slot></slot>
             </nav>
         </div>
